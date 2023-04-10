2cc30f6770827b0ac28300eb6c7539f8d4ff12ee
mukrime-s <mkrimesagrglu@gmail.com>
2023-04-10 20:28:14 +0300

    updated game and fixed bugs

--- XOX Game/xoxGame/Form1.cs
+++ XOX Game/xoxGame/Form1.cs
@@ -19,7 +19,6 @@ namespace xoxGame
         Button[] buttons; // butonlar dizisi
         int[] values;     // butonların durumunu tutan dizi (0: boş, 1: X, 2: O)
         bool isNextX = false; // bir sonraki resmin X mi O mu olacağını tutan değişken
-
         private void Reset()//oyunu basa alan fonsiyon yazildi.
         {
             for (int i = 0; i < 9; i++)
@@ -30,8 +29,8 @@ namespace xoxGame
         }
         private bool CheckWin(int player)
         {
-            // yatay kontrol
-            for (int i = 0; i < 9; i += 3)
+            
+            for (int i = 0; i < 9; i += 3)// yatay kontrol saglandi.
             {
                 if (values[i] == player && values[i + 1] == player && values[i + 2] == player)
                 {
@@ -39,8 +38,8 @@ namespace xoxGame
                 }
             }
 
-            // dikey kontrol
-            for (int i = 0; i < 3; i++)
+            
+            for (int i = 0; i < 3; i++)// dikey kontrol saglandi.
             {
                 if (values[i] == player && values[i + 3] == player && values[i + 6] == player)
                 {
@@ -61,14 +60,9 @@ namespace xoxGame
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            
             buttons = new Button[] { button1, button2, button3, button6, button5, button4, button9, button8, button7 };
             values = new int[9];
-            for (int i = 0; i < 9; i++)
-            {
-                values[i] = 0; // butonlar boş başlar
-                buttons[i].Image = null; // butonların resimlerini boşalt
-            }
+            Reset();
 
             // X ve O resimlerini yükle
             Image xImage = Image.FromFile(Application.StartupPath + @"../../../images/x.png");
@@ -96,50 +90,33 @@ namespace xoxGame
         {
             Button button = (Button)sender;
             int index = Array.IndexOf(buttons, button); // butonun dizideki indeksi
-            if (values[index] == 0) // boş ise
+            if (values[index] == 0) 
             {
-                if (isNextX)
-                {
-                    button.Image = Image.FromFile(Application.StartupPath + @"../../../images/x.png"); // butonun görüntüsünü X yap
-                    values[index] = 1; // butonun değerini 1 (X) yap
+                button.Image = Image.FromFile(Application.StartupPath + @"../../../images/x.png"); // butonun görüntüsünü X yap
+                values[index] = 1; 
 
-                    if (CheckWin(1))
-                    {
-                        MessageBox.Show("Tebrikler, kazandın!");
-                        Reset();
-                        return;
-                    }
-                }
-                else
+                if (CheckWin(1))
                 {
-                    button.Image = Image.FromFile(Application.StartupPath + @"../../../images/o.png"); // butonun görüntüsünü O yap
-                    values[index] = 2; // butonun değerini 2 (O) yap
-
-                    if (CheckWin(2))
-                    {
-                        MessageBox.Show("Maalesef kaybettin.");
-                        Reset();
-                        return;
-                    }
+                    MessageBox.Show("Tebrikler, kazandın!");
+                    Reset();
+                    return;
                 }
 
-                isNextX = !isNextX; // bir sonraki resmin tersini belirle
-
-                // Rastgele bir O yerleştirme işlemi
-                List<int> emptyIndices = new List<int>();
+                
+                List<int> emptyIndices = new List<int>();// Rastgele bir O yerleştirme işlemi
                 for (int i = 0; i < 9; i++)
                 {
                     if (values[i] == 0)
                     {
-                        emptyIndices.Add(i); // boş olan butonların indekslerini ekle
+                        emptyIndices.Add(i); 
                     }
                 }
-                if (emptyIndices.Count > 0) // en az bir boş buton varsa
+                if (emptyIndices.Count > 0) 
                 {
                     Random random = new Random();
-                    int randomIndex = emptyIndices[random.Next(emptyIndices.Count)]; // rastgele bir indeks seç
-                    buttons[randomIndex].Image = Image.FromFile(Application.StartupPath + @"../../../images/o.png"); // seçilen butonun görüntüsünü O yap
-                    values[randomIndex] = 2; // seçilen butonun değerini 2 (O) yap
+                    int randomIndex = emptyIndices[random.Next(emptyIndices.Count)];
+                    buttons[randomIndex].Image = Image.FromFile(Application.StartupPath + @"../../../images/o.png"); 
+                    values[randomIndex] = 2; 
 
                     if (CheckWin(2))
                     {
